b4f9c16278f080120bb28cc1454391d0bcb325c0
za9csergey <za9csergey@localhost>
2016-07-08 14:12:27 +0300

    ?????? ??????, ?????? ?????? ??????

---
+++ GenomProj/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GenomProj
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WriteableBitmap wb = new WriteableBitmap(
+                                    (int)img.Width,
+                                    (int)img.Height,
+                                    96,
+                                    96,
+                                    PixelFormats.Bgr32,
+                                    null);
+            img.Source = wb;
+        }
+    }
+}
